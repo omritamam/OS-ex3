b735b797a284a13249f623c48f256862a5c4f185
Otamam <otamam@brightsourceenergy.com>
2022-04-30 15:36:26 +0300

    saturaday at home

--- Context.h
+++ Context.h
@@ -4,6 +4,21 @@
 
 #ifndef EX3_CONTEXT_H
 #define EX3_CONTEXT_H
+using namespace std;
 
-class Context;
+class Context{
+    const MapReduceClient *client;
+    vector<InputPair> inputVec;
+    vector<OutputPair> outputVec;
+
+public:
+    Context(const MapReduceClient *const client, const vector<InputPair> inputVec, vector<OutputPair> outputVec) {
+        this->client = client;
+        this->inputVec = inputVec;
+        this->outputVec = outputVec;
+    }
+
+
+
+    };
 #endif //EX3_CONTEXT_H
